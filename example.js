6bb922fe59695d6411b8036fe05bf21570d09c21
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-04-27 17:48:39 +0530

    Add files via upload
    Uploaded example.js

---
+++ example.js
@@ -0,0 +1,90 @@
+var config = {
+  enableEventTracking: true,
+  // edeEndpoint: 'http://10.8.93.193:8080',
+  logLevel: 0,
+  templateConfiguration: {
+    carousel: {
+      // enablePriceValidation: true,
+      forceStarSmall: false,
+      showDoNotRecommend: true,
+      showPercentageOff: true,
+      showProductCode: true,
+      showStarRating: false,
+      showUserPreference: true
+    },
+    grid: {
+      // enablePriceValidation: true,
+      forceStarSmall: false,
+      horizontalCount: 2,
+      showDoNotRecommend: true,
+      showPercentageOff: true,
+      showProductCode: true,
+      showStarRating: false,
+      showUserPreference: true,
+      verticalCount: 2
+    },
+    pickUpWhereYouLeft: {
+      forceStarSmall: false,
+      showPercentageOff: true,
+      showProductCode: true,
+      showStarRating: false,
+      showUserPreference: true
+    },
+    recentCategory: {
+      // enablePriceValidation: true,
+      forceStarSmall: false,
+      showDoNotRecommend: true,
+      showPercentageOff: true,
+      showProductCode: true,
+      showStarRating: false,
+      showUserPreference: true
+    },
+    youViewed: {
+      // enablePriceValidation: true,
+      forceStarSmall: false,
+      showDoNotRecommend: true,
+      showPercentageOff: true,
+      showProductCode: true,
+      showStarRating: true,
+      showUserPreference: true
+    },
+    productComparison: {
+      // enablePriceValidation: true,
+      forceStarSmall: true,
+      limit: 5
+    }
+  }
+};
+
+var recommendationRenderer = new bd.RecommendationRenderer(config);
+recommendationRenderer.setApiKey('JAAso4n7ibZpbRxsIZyo2ccVt3E7np1C')
+  .setChannel('Demo')
+  .setPage('MyPage')
+  .setCcp('atgId', '2254000009113198')
+  .setCcp('productNumbers', '2213221')
+  .setCcp('rvProductNumbers', '1167424,2397792,2630235,2567107,825463,1431538,1944597,2116842,2116882,2330037,2410809,2476444,2519633,2531899,2554776,2563972,2565576,2575490,2590050,2618590,2637063')
+  .onProductClick(function (element, data) {
+    console.log('Product Click:', data);
+  })
+  .onSearchTermClick(function (element, data) {
+    console.log('Search Term Click:', data);
+  })
+  .onProductDislikeClick(function (element, data) {
+    console.log('Product Dislike Click:', data);
+  })
+  .onProductLikeClick(function (element, data) {
+    console.log('Product Like Click:', data);
+  })
+  .onProductNoPreferenceClick(function (element, data) {
+    console.log('Product No Preference Click:', data);
+  })
+  .onProductDoNotRecommendClick(function (element, data) {
+    console.log('Product Do Not Recommend Click:', data);
+  })
+  .onProductUserReviewClick(function (element, data) {
+    console.log('Product User Review Submit Click:', data);
+  })
+  .onProductCartAddClick(function (element, data) {
+    console.log('Product Cart Add Click:', data);
+  })
+  .injectCombinedRecommendations();
