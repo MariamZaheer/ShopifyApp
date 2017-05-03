cd229f47864f38aab18a0c2b585d7c4ba25ce5b0
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-05-03 11:52:58 +0530

    Update AddDiv.js

--- AddDiv.js
+++ AddDiv.js
@@ -1,33 +1,13 @@
-// (function(){
-
-// console.log("Before onload");
-
-// // window.onload = function() {
-
-
-
-//     console.log("Inside onload");
-// 	var newDiv = document.createElement('div');
-// 	newDiv.id = 'bd_rec_Horizontal';
-// 	newDiv.dataTemplateType = 'carousel';
-	
-// 	if (document.body != null) { 
-// 	    document.getElementByTagName('body')[0].appendChild('div'); 
-// 	    console.log("Div added");
-// 	}
-// // };
-    
-// })();
-
-
 $( document ).ready(function() {
 
 	function result(text) {
 		alert("success");
 	};
 
-	var text = '[{"channel":"Shopify","pageType":"Cart","placement":"Horizontal2","uiElement":"PGRpdiBpZD0iYmRfcmVjX2hvcml6b250YWwyIiBkYXRhLXRlbXBsYXRlLXR5cGU9ImNhcm91c2VsIj48L2Rpdj4="},{"channel":"Shopify","pageType":"Home","placement":"Grid","uiElement":"PGRpdiBpZD0iYmRfcmVjX2dyaWQiIGRhdGEtdGVtcGxhdGUtdHlwZT0iZ3JpZCI+PC9kaXY+="}]';
+	// JSON string containing the hardcoded BP configurations.
+	var text = [{"channel":"Shopify","pageType":"Cart","placement":"Horizontal2","uiElement":"PGRpdiBpZD0iYmRfcmVjX2hvcml6b250YWwyIiBkYXRhLXRlbXBsYXRlLXR5cGU9ImNhcm91c2VsIj48L2Rpdj4="},{"channel":"Shopify","pageType":"Home","placement":"Grid","uiElement":"PGRpdiBpZD0iYmRfcmVjX2dyaWQiIGRhdGEtdGVtcGxhdGUtdHlwZT0iZ3JpZCI+PC9kaXY+="}];
 
+	// Ajax call to get configuration from BP.
 	$.ajax({
 		url: "https://httpbin.org/get",
 		success: function(jsonFromCheyenne) {
@@ -37,20 +17,20 @@ $( document ).ready(function() {
 		}
 	});
 
-    console.log( "Div - ready!" );
-    
-//     var $div = $('<div />').appendTo('body');
-    var container = $("#home-imagebox");
-    var $div = $('<div />').appendTo(container);
-    $div.attr('id', 'bd_rec_Grid');
-    $div.attr('data-template-type', 'carousel');
-    console.log( "Div - Done!" );
+	console.log( "Div - ready!" );
+	
+	// Adding a div tag.
+	var container = $("#home-imagebox");
+	var $div = $('<div />').appendTo(container);
+	$div.attr('id', 'bd_rec_Grid');
+	$div.attr('data-template-type', 'carousel');
+	console.log( "Div - Done!" );
 
-    console.log( "Script - ready!" );
-    var $script = document.createElement("script");
-    $script.type = "text/javascript";
-    $script.src = "https://rawgit.com/MariamZaheer/ShopifyApp/master/example.js";
-    // Use any selector
-    $("body").append($script);
-    console.log( "Script - Done!" );
+	// Loading a sample recommendations js.
+	console.log( "Script - ready!" );
+	var $script = document.createElement("script");
+	$script.type = "text/javascript";
+	$script.src = "https://rawgit.com/MariamZaheer/ShopifyApp/master/example.js";
+	$("body").append($script);
+	console.log( "Script - Done!" );
 });
