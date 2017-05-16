3e364e03431c50b5e3eb3319d4c192cb4da599eb
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-05-16 17:04:33 +0530

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -37,18 +37,33 @@ $( document ).ready(function() {
 // 	$div.attr('data-template-type', 'carousel');
 // 	console.log( "Div - Done!" );
 
-	// Loading the basic recommendations js by default.
-	console.log( "Script - ready!" );
-	var $script = document.createElement("script");
-	$script.type = "text/javascript";
-	$script.src = "https://rawgit.com/MariamZaheer/ShopifyApp/master/basic.js";
-	$("body").append($script);
-	console.log( "Script - Done!" );
+// 	// Loading the basic recommendations js by default.
+// 	console.log( "Script - ready!" );
+// 	var $script = document.createElement("script");
+// 	$script.type = "text/javascript";
+// 	$script.src = "https://rawgit.com/MariamZaheer/ShopifyApp/master/basic.js";
+// 	$("body").append($script);
+// 	console.log( "Script - Done!" );
+	
+	// Adding a div tag.
+	var container = $(".kickstarter__meta");
+	var $div = $('<div />').appendTo(container);
+	$div.attr('id', 'bd_rec_Grid');
+	$div.attr('data-template-type', 'carousel');
+	console.log( "Div - Done!" );
 	
 	console.log( "Script - ready!" );
 	var $script = document.createElement("script");
 	$script.type = "text/javascript";
 	$script.src = "https://rawgit.com/MariamZaheer/ShopifyApp/master/bd-experience-rendering-sdk.min.js";
+	$(".kickstarter__meta").append($script);
+	console.log( "Script - Done!" );
+	
+	// Loading the basic recommendations js by default.
+	console.log( "Script - ready!" );
+	var $script = document.createElement("script");
+	$script.type = "text/javascript";
+	$script.src = "https://rawgit.com/MariamZaheer/ShopifyApp/master/basic.js";
 	$(".kickstarter__meta").append($script);
 	console.log( "Script - Done!" );
 });
