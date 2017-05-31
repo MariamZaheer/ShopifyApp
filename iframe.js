622a0c118cc9f9947342e1e8937744be04266b50
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-05-31 17:19:36 +0530

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,17 +1,18 @@
 <script type='text/javascript'>
   // If the current window is the 'parent', change the URL by setting location.href
-  if (window.top == window.self) {
-    window.top.location.href = "http://prometheus-asgard.myshopify.com/admin/oauth"
-    // "/auth/shopify?shop=myshopname";
+//   if (window.top == window.self) {
+//     window.top.location.href = "http://prometheus-asgard.myshopify.com/admin/oauth"
+//     // "/auth/shopify?shop=myshopname";
 
-  // If the current window is the 'child', change the parent's URL with postMessage
-  } else {
-    message = JSON.stringify({
-      message: "Shopify.API.remoteRedirect",
-      data: { location: window.location.origin + "http://prometheus-asgard.myshopify.com/admin/oauth" }
-        // "/auth/shopify?shop=myshopname" }
-    });
-    window.parent.postMessage(message, "https://prometheus-asgard.myshopify.com"):
-      // "https://myshopname.myshopify.com");
-  }
+//   // If the current window is the 'child', change the parent's URL with postMessage
+//   } else {
+//     message = JSON.stringify({
+//       message: "Shopify.API.remoteRedirect",
+//       data: { location: window.location.origin + "http://prometheus-asgard.myshopify.com/admin/oauth" }
+//         // "/auth/shopify?shop=myshopname" }
+//     });
+//     window.parent.postMessage(message, "https://prometheus-asgard.myshopify.com"):
+//       // "https://myshopname.myshopify.com");
+//   }
+  response.headers["X-Frame-Options"] = "ALLOW-FROM https://#{shop}"
 </script>
