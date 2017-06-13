69f845b63d655c8113eccebaeba54fc4d58ae159
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-06-13 12:34:11 +0530

    Create function.js

--- function.js
+++ function.js
@@ -1,1 +1,17 @@
+$( document ).ready(function() {
+	
+	var queryString = window.location.href;
+	console.log(queryString);
 
+	var variables = queryString.split("&");
+	console.log(variables);
+	
+	var planVariables = variables[0];
+	console.log(planVariables);
+	
+	var planType = planVariables.split("=");
+	console.log(planType);
+	var plan = planType[1];
+	console.log(plan);
+	
+});
