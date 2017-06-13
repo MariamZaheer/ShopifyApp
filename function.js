237575b857ce4f037552034502cb855d3443154c
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-06-13 12:39:53 +0530

    Create function.js

--- function.js
+++ function.js
@@ -1,17 +1,10 @@
 $( document ).ready(function() {
 	
 	var queryString = window.location.href;
-	console.log(queryString);
+	
+	var search = queryString.split("=");
 
-	var variables = queryString.split("&");
-	console.log(variables);
-	
-	var planVariables = variables[0];
-	console.log(planVariables);
-	
-	var planType = planVariables.split("=");
-	console.log(planType);
-	var plan = planType[1];
-	console.log(plan);
+	var searchTerm = search[1];
+	alert("Search Term is : " + searchTerm);
 	
 });
