2dd01dc4e68dd982ea1ee96a2e58d92707490a75
MariamZaheer <MariamZaheer@users.noreply.github.com>
2017-06-27 11:18:49 +0530

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -152,7 +152,7 @@ $( document ).ready(function() {
 	// Collections page
 	if (top.location.pathname === '/collections') {
 		var currentURL = (document.URL);
-		var part = currentURL.split("/")[1];
+		var part = currentURL.split("/");
 		console.log("Collections : " + part);
 		alert("Collections");
 		
